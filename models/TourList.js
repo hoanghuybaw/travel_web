f26e89df467a64b20b06da089b6f3151dd9b4d37
hoang huy <hoanghuybaw@gmail.com>
2023-06-19 12:47:02 +0700

    delete tour

--- models/TourList.js
+++ models/TourList.js
@@ -60,6 +60,7 @@ export default mongoose.model('TourList',
             type: String,
             required: true,
         },
+        selectedDate: [String],
         images: [String]
     })
 )
--- routes/tourlist.js
+++ routes/tourlist.js
@@ -5,34 +5,47 @@ import { TourList } from "../models/index.js";
 import multer from "multer";
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, 'public/uploads/');
-    },
-    filename: (req, file, cb) => {
-      const filename = Date.now() + '-' + file.originalname;
-      cb(null, filename);
-    },
-  });
-  
+  destination: (req, file, cb) => {
+    cb(null, "public/uploads/");
+  },
+  filename: (req, file, cb) => {
+    const filename = Date.now() + "-" + file.originalname;
+    cb(null, filename);
+  },
+});
+
 const upload = multer({ storage });
 
 router.get("/", tourlistController.getListTourlist);
 
-router.get("/:id",  (req, res) => {
-    const tourId = req.params.id;
-  
-    TourList.findById(tourId, (err, tour) => {
-      if (err) {
-        res.status(500).json({ error: err.message });
-      } else if (!tour) {
-        res.status(404).json({ error: 'Tour not found' });
-      } else {
-        res.json(tour);
-      }
-    });
-  });
+router.get("/:id", (req, res) => {
+  const tourId = req.params.id;
 
-router.post("/insert", upload.array('images', 5), (req, res) => {
+  TourList.findById(tourId, (err, tour) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    } else if (!tour) {
+      res.status(404).json({ error: "Tour not found" });
+    } else {
+      res.json(tour);
+    }
+  });
+});
+router.delete("/:id", (req, res) => {
+  const tourId = req.params.id;
+
+  TourList.findByIdAndDelete(tourId, (err, tour) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    } else if (!tour) {
+      res.status(404).json({ error: "Tour not found" });
+    } else {
+      res.json({ message: "Tour deleted successfully" });
+    }
+  });
+});
+
+router.post("/insert", upload.array("images", 5), (req, res) => {
   const {
     name,
     destination,
@@ -46,6 +59,7 @@ router.post("/insert", upload.array('images', 5), (req, res) => {
     night,
     priceChildren,
     thumbNail,
+    selectedDate,
   } = req.body;
   const images = req.files.map((file) => file.filename);
   const newTour = new TourList({
@@ -61,14 +75,15 @@ router.post("/insert", upload.array('images', 5), (req, res) => {
     night,
     priceChildren,
     thumbNail,
+    selectedDate,
     images,
   });
   newTour.save((err, tour) => {
     if (err) {
-        debugger
+      debugger;
       res.status(500).json({ success: false, message: err.message });
     } else {
-      res.json({ success: true, message: "Thêm mới tour thành công", tour });
+      res.json({ success: true, message: "Tour inser success", tour });
     }
   });
 });
