7c9f4d029667dd0e1e55eb68d88756f2ba2a2549
hoang huy <hoanghuybaw@gmail.com>
2023-07-23 22:48:49 +0700

    auth

--- authentication/auth.js
+++ authentication/auth.js
@@ -2,16 +2,21 @@ import HttpStatusCode from "../exception/HttpStatusCode.js";
 import Jwt from "jsonwebtoken";
 
 export default function checkToken(req, res, next) {
-  const url = req.url 
-  const url_index = url.indexOf('reset-password/') + 'reset-password/'.length;
-  let url_string = url.slice(url_index);
+  const url = req.url;
+  const url_index_password = url.indexOf("reset-password/") + "reset-password/".length;
+  const url_index_tourlist = url.indexOf("tourlist/") + "tourlist/".length;
+  let url_string_password = url.slice(url_index_password);
+  let url_string_tourlist = url.slice(url_index_tourlist);
   // bypass login, register
   if (
     req.url.toLowerCase().trim() == "/users/login".toLowerCase().trim() ||
     req.url.toLowerCase().trim() == "/users/register".toLowerCase().trim() ||
     req.url.toLowerCase().trim() == "/tourlist".toLowerCase().trim() ||
-    req.url.toLowerCase().trim() == "/users/forgot-password".toLowerCase().trim() ||
-    req.url.toLowerCase().trim() == `/users/reset-password/${url_string}`.toLowerCase().trim()
+    req.url.toLowerCase().trim() == `/tourlist/${url_string_tourlist}`.toLowerCase().trim() ||
+    req.url.toLowerCase().trim() ==
+      "/users/forgot-password".toLowerCase().trim() ||
+    req.url.toLowerCase().trim() ==
+      `/users/reset-password/${url_string_password}`.toLowerCase().trim()
   ) {
     next();
     return;
